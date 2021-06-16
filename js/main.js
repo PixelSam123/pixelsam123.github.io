4b76d51e2e8bf3dbabb7fe3a8923156aa402adc5
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-06-16 15:34:54 +0700

    Prevent errors for OS recognizer

--- js/main.js
+++ js/main.js
@@ -35,7 +35,10 @@ applyLookMode();
 // Choose window style for IntelliJ look pro tip
 if (navigator.appVersion.indexOf("Win") != -1) {
   const windowElements = body.getElementsByClassName("window");
-  for (let index = 0; index < windowElements.length; index++) {
-    windowElements[index].className = "window windows10";
+  const windowElementsCount = windowElements.length;
+  if (windowElementsCount > 0) {
+    for (let index = 0; index < windowElementsCount; index++) {
+      windowElements[index].className = "window windows10";
+    }
   }
 }

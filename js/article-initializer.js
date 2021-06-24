c35e714d5994f9e926242e58f2270eab0fbef143
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-06-24 18:27:47 +0700

    Remove placeholder text, add latest articles section to homepage

--- js/article-initializer.js
+++ js/article-initializer.js
@@ -4,14 +4,16 @@ fetch("../resources/article-content-list.json")
   .then((response) => response.json())
   .then((fetchedArticleContentList) => {
     const articleHeader = document.getElementById("article-header");
-    articleHeader.getElementsByTagName("h1")[0].innerText = fetchedArticleContentList.header_title;
+    articleHeader.getElementsByTagName("h1")[0].innerText =
+      fetchedArticleContentList[0].header_title;
     articleHeader.getElementsByTagName("p")[0].innerText =
-      fetchedArticleContentList.header_description;
-    articleHeader.getElementsByTagName("img")[0].src = fetchedArticleContentList.header_image_url;
+      fetchedArticleContentList[0].header_description;
+    articleHeader.getElementsByTagName("img")[0].src =
+      fetchedArticleContentList[0].header_image_url;
 
     const articleSections = body.getElementsByClassName("article-section");
     const subheaderLinks = body.getElementsByClassName("subheader-link");
-    const fetchedArticleSections = fetchedArticleContentList.sections;
+    const fetchedArticleSections = fetchedArticleContentList[0].sections;
     duplicateElement(articleSections[0], fetchedArticleSections.length - 1);
     duplicateElement(subheaderLinks[0], fetchedArticleSections.length - 1);
 
@@ -50,5 +52,5 @@ fetch("../resources/article-content-list.json")
         executionIsAllowed = false;
       }
     });
-    setInterval(() => (executionIsAllowed = true), 20);
+    setInterval(() => (executionIsAllowed = true), 10);
   });

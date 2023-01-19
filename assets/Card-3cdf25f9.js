9ba17762dcd690da2320e5ec0dea7175e4cd97f3
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-01-19 22:16:28 +0700

    deployment with some fixes

--- assets/Card-3cdf25f9.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,o,a as s,T as t}from"./app-1f970e42.js";const c={},a={class:"p-2 rounded-xl border-4 hover:border-black dark:hover:border-slate-300"};function n(e,d){return o(),s("div",a,[t(e.$slots,"default")])}const _=r(c,[["render",n],["__file","C:/Users/pkupr/Documents/repos/self-site/src/components/Card.vue"]]);export{_ as C};

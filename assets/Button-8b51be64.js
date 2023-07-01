18a7ec120310f4744a0e82180266882205cd6bfb
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-07-01 16:27:00 +0700

    deployment with repo preview scroll jumping fix

---
+++ assets/Button-8b51be64.js
@@ -0,0 +1,3 @@
+import{g as n,f as r,a,r as s,t as o,k as l,b as c}from"./entry-client-b385ce8d.js";const i=o("<button>");function u(t){return(()=>{const e=n(i);return e.$$click=()=>t.onClick?.(),r(e,()=>t.children),a(()=>c(e,`w-full rounded-lg border-[3px] border-[#CF5C36] px-2 py-1
+        font-semibold transition duration-100 hover:bg-[#CF5C36]
+        hover:text-white active:scale-95 ${t.class??""}`)),s(),e})()}l(["click"]);export{u as B};

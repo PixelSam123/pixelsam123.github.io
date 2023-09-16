1478c673d5199c0d70adb4ec80c5e9459939421b
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-09-16 11:00:18 +0700

    swap pcp with web-based frontend

---
+++ pcp/_next/static/cN8pqXs073FdBcAp5Tl2B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-135022fb237e8464.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ pcp/_next/static/chunks/pages/_app-e3a45f45b4fcedf8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{2137:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(2897)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(2137),_(3456)}),_N_E=n.O()}]);

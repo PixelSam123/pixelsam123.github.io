b0129803e29a58fc841c5c650ccaa775d503c220
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-09-23 19:23:08 +0700

    personal site updates

---
+++ assets/Card-db43822a.js
@@ -0,0 +1,2 @@
+import{g as t,f as a,a as o,t as s,b as n}from"./entry-client-5daf0763.js";const d=s("<div>");function c(r){return(()=>{const e=t(d);return a(e,()=>r.children),o(()=>n(e,`rounded-lg border-[3px] p-2 transition-colors duration-100
+        hover:border-black dark:hover:border-slate-300 ${r.class}`)),e})()}export{c as C};

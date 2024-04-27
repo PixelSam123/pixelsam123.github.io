13aa3bcb5c156e71fc57dae05f4b6e7ed61c5b48
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2024-04-27 12:21:45 +0700

    fixes

---
+++ _astro/Icon.DuGAHZE3.js
@@ -0,0 +1,6 @@
+import{n as u,g as d,p as c,m as o,a as h,i as m,k as a,D as g,F as k,r as p,t as w}from"./web.Cdr1opmF.js";/**
+* @license lucide-solid v0.373.0 - ISC
+*
+* This source code is licensed under the ISC license.
+* See the LICENSE file in the root directory of this source tree.
+*/var v={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"},t=v,b=w("<svg>"),f=r=>r.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),N=r=>{const[e,n]=u(r,["color","size","strokeWidth","children","class","name","iconNode","absoluteStrokeWidth"]);return(()=>{var s=d(b);return c(s,o(t,{get width(){return e.size??t.width},get height(){return e.size??t.height},get stroke(){return e.color??t.stroke},get"stroke-width"(){return h(()=>!!e.absoluteStrokeWidth)()?Number(e.strokeWidth??t["stroke-width"])*24/Number(e.size):Number(e.strokeWidth??t["stroke-width"])},get class(){return`lucide lucide-${f(e?.name??"icon")} ${e.class!=null?e.class:""}`}},n),!0,!0),m(s,a(k,{get each(){return e.iconNode},children:([i,l])=>a(g,o({component:i},l))})),p(),s})()},z=N;export{z as I};

a331bc18afd23251bc5ad5a7aa3865975880f104
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2024-07-20 09:00:41 +0700

    update gws

--- gws/_next/static/chunks/app/layout-47b2c63a99aadb74.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7609:function(n,e,u){Promise.resolve().then(u.t.bind(u,3054,23))},3054:function(){}},function(n){n.O(0,[141,971,23,744],function(){return n(n.s=7609)}),_N_E=n.O()}]);
